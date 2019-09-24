f6416b695fb931e7da2f2984cefb9b90e6a95eea
IcemanZero <juancarlosmadrigalv@gmail.com>
2019-09-24 15:33:40 -0600

    listo

--- Proyecto_datos_2019/main.cpp
+++ Proyecto_datos_2019/main.cpp
@@ -15,18 +15,10 @@ int main() {
 	s0->push(23);
 	s0->push(12);
 	cout << "s0: " << s0->toString() << endl;
-	//s0->~Stack();
-	/*
-	Integer<double>* _myInteger = new Integer<double>(73187498132748917);/
-	*/
 	Integer<int>* _myInteger = new Integer<int>();
 	_myInteger->getLista()->agregar(s0);
-	//Integer<int>* _mySecondInteger = nullptr;	
 	cout << _myInteger->toString() << endl;
 	cout << _myInteger->getLista()->toString() << endl;
-	//_mySecondInteger = _myInteger;
-	//cout << _myInteger->toString() << endl;
-	//cout << _mySecondInteger->toString() << endl;
 	delete _myInteger;
 	return 0;
 }
