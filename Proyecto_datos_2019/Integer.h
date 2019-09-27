8eda9770199efa68463c2f90d957b6b4ea63431e
IcemanZero <juancarlosmadrigalv@gmail.com>
2019-09-27 15:46:43 -0600

    Documentacion del codigo

--- Proyecto_datos_2019/Integer.h
+++ Proyecto_datos_2019/Integer.h
@@ -8,12 +8,18 @@
 using namespace std;
 class Integer : public ObjetoBase{
 public:
+	//Constructor por defecto
 	Integer();
+	//Constructor que recibe el numero a almacenar como un string
+	//y luego lo va almacenando por partes en la lista<stack<int>>
 	Integer(string x);
+	//Constructor que recibe una Lista, no se utiliza mucho.
 	Integer(Lista<Stack<int>>*);
-
+	//Destructor
 	virtual ~Integer();
+	//Sobrecarga del operador=
 	virtual Integer& operator=(const Integer&);
+	//Sobrecarga del operador+
 	virtual Integer* operator+(const Integer&);
 	//virtual Integer& operator==(const Integer&);
 	//Integer(const Integer& orig);
@@ -36,15 +42,23 @@ public:
 	//    virtual Integer& operator<<(const Integer&);
 	//    virtual long* parse(string);
 	//    virtual int* parse(string);
+
+	//Metodos accesores y mutadores
 	virtual void setLista(Lista<Stack<int>>*);
 	virtual Lista<Stack<int>>* getLista();
+	string getValor()const;
+	//Metodo toString()
 	virtual string toString()const;
 
-	//Mas metodos...
+	//...Mas metodos...
+	//Este metodo se encarga de calcular el numero optimo de ranuras X stack
 	int calcularNumeroOptimoDeRanuras(int _tam);
+	//Este metodo se encarga de calcular el numero optimo de stacks X Lista
 	int calcularNumeroOptimoDeStacks(int _tam);
+	//Este metodo se encarga de detallar el desperdicio de memoria en la implementacion
+	//de un objeto Integer.
 	string getDetalles();
-	string getValor()const;
+	
 private:
 	Lista<Stack<int>>* _lista;
 	string _valor;
